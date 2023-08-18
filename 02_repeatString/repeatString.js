6c43a5ba38c1daaaa59471d6de01214b56d13af0
menvqeta <34645476+menvqeta@users.noreply.github.com>
2023-08-18 14:02:06 +0530

    Added till 3rd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToBeRepeated, numOfRepetitions) {
+    let ans = '';
+    if(numOfRepetitions<0) {
+        ans = 'ERROR';
+        return ans;
+    }
+    for(let i=0; i<numOfRepetitions; i++) {
+        ans+=stringToBeRepeated;
+    }
+    return ans;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(originalWord) {
+    const size = originalWord.length;
+    let reversed = '';
+    for(let i=size-1; i>=0; i--) {
+        reversed += originalWord[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
