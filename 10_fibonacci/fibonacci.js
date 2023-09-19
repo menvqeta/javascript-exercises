15282102f1a6008545a39fa41f685dfafadb530d
menvqeta <34645476+menvqeta@users.noreply.github.com>
2023-09-19 19:41:36 +0530

    Added ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    if(num<0) {
+        return "OOPS";
+    }
+    return fib(num);
+    
 };
+
+function fib(num) {
+    let arr = [];
+    arr[0] = 1;
+    arr[1] = 1;
+    for(let i=2; i<=num; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[num-1];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
