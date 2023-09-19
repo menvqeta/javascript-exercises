7d5bcf50194a2c707e78bb8c66b38437c8dca38b
menvqeta <34645476+menvqeta@users.noreply.github.com>
2023-09-19 19:27:38 +0530

    Corrected ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let convWord=``;
+    let reverse = '';
+    for(let i=0; i<str.length; i++) {
+        c = str[i];
+        if(c == ' ') {
+            continue;
+        }
+        if(isAlpha(c)) {
+            c = c.toLowerCase();
+            convWord +=c;
+        }
+        else if(isNumeric(c)) {
+            convWord +=c;
+        }
+    }
+    for(let i=convWord.length-1; i>=0; i--) {
+        reverse += convWord[i];
+    }
+    return reverse === convWord;
 };
+
+function isAlpha(c) {
+    return (('a'<=c) && (c<='z')) || (('A'<=c) && (c<='Z'));
+}
+
+function isNumeric(c) {
+    return 0<=c && c<=9;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
