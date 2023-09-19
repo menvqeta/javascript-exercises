2aba083da8bc39d5ea03d1b062748d90427cc849
menvqeta <34645476+menvqeta@users.noreply.github.com>
2023-09-19 18:59:49 +0530

    Completed calc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return +a + +b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return +a - +b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let s = 0;
+  for(let i=0; i<arr.length; i++) {
+    s+=arr[i];
+  }
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let m = 1;
+  for(let i=0; i<arr.length; i++) {
+    m*=arr[i];
+  }
+  return m;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  let m =1;
+  for(let i=1; i<=pow; i++) {
+    m*=num;
+  }
+  return m;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let m=1;
+  for(let i=1; i<=num; i++) {
+    m*=i;
+  }
+  return m;
 };
 
 // Do not edit below this line
