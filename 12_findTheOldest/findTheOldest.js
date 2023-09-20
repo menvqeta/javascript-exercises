6f6bcb6083169b9c802047d2ef7181c46f3a959e
menvqeta <34645476+menvqeta@users.noreply.github.com>
2023-09-20 18:40:20 +0530

    Added ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort(
+        function(a, b) {
+            return getAge(b) - getAge(a);
+            
+        }
+    );
+    return people[0];
 };
+
+function getAge(person) {
+    let currentYear = new Date().getFullYear();
+    if ('yearOfDeath' in person) {
+        currentYear = person['yearOfDeath'];
+    }
+    let age = currentYear - person['yearOfBirth'];
+    return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
