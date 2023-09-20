9a0745a0c7993964ac8a8efb0bacb724c5407074
menvqeta <34645476+menvqeta@users.noreply.github.com>
2023-09-20 18:30:24 +0530

    Added ex 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map(
+        function(item) {
+            return item['title'];
+        }
+    );
 };
 
 // Do not edit below this line
